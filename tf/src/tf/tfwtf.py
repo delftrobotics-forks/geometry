f0625e921f79ce8b9b8c3c815ff8af79eee95e9f
Maarten de Vries <maarten@de-vri.es>
2016-05-06 16:27:13 +0200

    pytf: Use python3 compatible print function in tfwtf.py.

--- tf/src/tf/tfwtf.py
+++ tf/src/tf/tfwtf.py
@@ -31,6 +31,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # Revision $Id$
+from __future__ import print_function
 
 import time
 
@@ -178,11 +179,11 @@ def roswtf_plugin_online(ctx):
     if not is_tf_active():
         return
     
-    print "running tf checks, this will take a second..."
+    print("running tf checks, this will take a second...")
     sub1 = rospy.Subscriber('/tf', tf.msg.tfMessage, _tf_handle)
     time.sleep(1.0)
     sub1.unregister()
-    print "... tf checks complete"    
+    print("... tf checks complete")
 
     for r in tf_warnings:
         warning_rule(r, r[0](ctx), ctx)
